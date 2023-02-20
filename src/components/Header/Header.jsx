b6e1f91c160f032fc743e19f5082f68ae744e010
smerch88 <maxarmyk@gmail.com>
2023-02-20 11:36:52 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,52 @@
-import React, { useState } from "react"
-import { StyledAppBar, StyledIconButton, Link, LanguageDropDown } from "./styles";
-import BurgerIcon from '../../images/icons/Burger.svg'
-import SignupIcon from '../../images/icons/Sign up.svg'
-import ArrowDownIcon from '../../images/icons/Arrow_down.svg'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import {
+  StyledAppBar,
+  StyledIconButton,
+  Link,
+  LanguageDropDown,
+} from './styles';
+import BurgerIcon from '../../images/icons/Burger.svg';
+import SignupIcon from '../../images/icons/Sign up.svg';
+import ArrowDownIcon from '../../images/icons/Arrow_down.svg';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import { Box, Typography } from "@mui/material"
-import { useNavigate } from 'react-router-dom'
+import { Box, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 export const Header = () => {
-    const theme = useTheme();
-    const navigate = useNavigate();
-    const [activeLang, setActiveLang] = useState('EN');
-    const [navbarOpen, setNavbarOpen] = useState(false)
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const [activeLang, setActiveLang] = useState('EN');
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const isMobileVersion = useMediaQuery(theme.breakpoints.between('xs', 'lg'));
-    function handleToggle() {
-        setNavbarOpen(!navbarOpen)
-    }
+  const isMobileVersion = useMediaQuery(theme.breakpoints.between('xs', 'lg'));
+  function handleToggle() {
+    setNavbarOpen(!navbarOpen);
+  }
 
-    function redirectToSignIn() {
-        navigate(`/sign-in`);
-    } 
+  function redirectToSignIn() {
+    navigate(`/sign-in`);
+  }
 
-    // const langs = ['EN', 'RU', 'UA']; // get from redux
-    return (
-        <>
-
-
-            <StyledAppBar maxWidth={false}>
-                <Link to='/'>
-                    <Typography variant="h2">
-                        Techgen
-                    </Typography>
-                </Link>
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                    {/* language panel */}
-                   <LanguageDropDown src={ArrowDownIcon}/>
-                    {
-                        (isMobileVersion)
-                            ?
-                            // burger
-                            (<StyledIconButton src={BurgerIcon} handler={handleToggle} />)
-                            :
-                            //login
-                            (<StyledIconButton src={SignupIcon} handler={redirectToSignIn} />)
-                    }
-                </Box>
-            </StyledAppBar>
-
-
-        </>
-    )
-}
+  // const langs = ['EN', 'RU', 'UA']; // get from redux
+  return (
+    <>
+      <StyledAppBar maxWidth={false}>
+        <Link to="/">
+          <Typography variant="h2">Techgen</Typography>
+        </Link>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {/* language panel */}
+          <LanguageDropDown src={ArrowDownIcon} />
+          {isMobileVersion ? (
+            // burger
+            <StyledIconButton src={BurgerIcon} handler={handleToggle} />
+          ) : (
+            //login
+            <StyledIconButton src={SignupIcon} handler={redirectToSignIn} />
+          )}
+        </Box>
+      </StyledAppBar>
+    </>
+  );
+};
