12739285ce80c2989e559e5cbeca4eb627c74c08
Maryna Bazylevska <marynabazylevska@gmail.com>
2023-05-12 19:58:58 +0300

    refactor: refactor sidebar

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,56 +1,51 @@
 import { styled } from '@mui/system';
-import { Box, AppBar, IconButton, Typography } from "@mui/material"
+import { Box, AppBar, IconButton, Typography } from '@mui/material';
 import { Link as ReactLink } from 'react-router-dom';
-import theme from "styles/theme";
-export const StyledAppBar = styled(AppBar)(({
-    position: "static",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    [theme.breakpoints.up('xs')]: {
-        padding: '0 16px',
-    },
-    [theme.breakpoints.up('md')]: {
-        padding: '0 24px',
-    },
-    [theme.breakpoints.up('lg')]: {
-        padding: '0 32px',
-    },
-    [theme.breakpoints.up('xl')]: {
-        padding: '0 64px',
-    }
-}))
+import theme from 'styles/theme';
+export const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  position: 'absolute',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  zIndex: theme.zIndex.drawer + 1,
+  [theme.breakpoints.up('xs')]: {
+    padding: '0 16px',
+  },
+  [theme.breakpoints.up('md')]: {
+    padding: '0 24px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    padding: '0 32px',
+  },
+  [theme.breakpoints.up('xl')]: {
+    padding: '0 64px',
+  },
+}));
 
-export const LanguageDropDown = styled((props) => (
-    <Box {...props}>
-        <Typography variant="h2" >
-            EN
-        </Typography>
-        <Box
-            width="0.7em"
-            component="img"
-            src={props.src}
-
-        />
-    </Box>
-))(({
-    display: "flex", paddingRight: "1vw", marginRight: "1vw", borderRight: " 3px solid white ", zIndex: 100, '&:hover': {
-        cursor: "pointer",
-    }
-}))
+export const LanguageDropDown = styled(props => (
+  <Box {...props}>
+    <Typography variant="h2">EN</Typography>
+    <Box width="0.7em" component="img" src={props.src} />
+  </Box>
+))({
+  display: 'flex',
+  paddingRight: '1vw',
+  marginRight: '1vw',
+  borderRight: ' 3px solid white ',
+  zIndex: 100,
+  '&:hover': {
+    cursor: 'pointer',
+  },
+});
 
 export const Link = styled(ReactLink)({
-    textDecoration: 'none',
-    color: theme.palette.text.primary,
+  textDecoration: 'none',
+  color: theme.palette.text.primary,
 });
 
 export const StyledIconButton = styled(props => (
-    <IconButton onClick={props.handler}>
-        <Box
-            
-            component="img"
-            src={props.src}
-        />
-    </IconButton>
-))()
+  <IconButton onClick={props.handler}>
+    <Box component="img" src={props.src} />
+  </IconButton>
+))();
